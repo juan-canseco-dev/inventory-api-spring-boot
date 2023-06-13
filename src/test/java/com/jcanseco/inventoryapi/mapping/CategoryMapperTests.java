ab8062cdba3524637f93d254beaf822bf1f35dff
Juan Pablo Canseco Rios <juan.canseco.rios@gmail.com>
2023-06-13 16:32:54 -0600

    Category Repository & Mapper Tests

---
+++ src/test/java/com/jcanseco/inventoryapi/mapping/CategoryMapperTests.java
@@ -0,0 +1,32 @@
+package com.jcanseco.inventoryapi.mapping;
+
+import com.jcanseco.inventoryapi.dtos.CreateCategoryDto;
+import com.jcanseco.inventoryapi.entities.Category;
+import com.jcanseco.inventoryapi.mappers.CategoryMapper;
+import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CategoryMapperTests {
+    private final CategoryMapper mapper = Mappers.getMapper(CategoryMapper.class);
+
+    @Test
+    public void entityToDto() {
+        assertNotNull(mapper);
+
+        var entity = Category.builder().id(1L).name("Electronics").build();
+        var dto = mapper.entityToDto(entity);
+
+        assertEquals(entity.getId(), dto.getId());
+        assertEquals(entity.getName(), dto.getName());
+    }
+
+    @Test
+    public void createDtoToEntity() {
+        assertNotNull(mapper);
+        var dto = new CreateCategoryDto("Electronics");
+        var entity = mapper.createDtoToEntity(dto);
+        assertEquals(dto.getName(), entity.getName());
+    }
+
+}
