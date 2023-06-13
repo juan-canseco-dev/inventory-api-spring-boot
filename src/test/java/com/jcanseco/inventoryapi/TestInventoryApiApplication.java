761889714d7dfbcc13024006b3e0daeaf9b1a987
Juan Pablo Canseco Rios <juan.canseco.rios@gmail.com>
2023-06-13 06:49:28 -0600

    first commit

---
+++ src/test/java/com/jcanseco/inventoryapi/TestInventoryApiApplication.java
@@ -0,0 +1,22 @@
+package com.jcanseco.inventoryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.MySQLContainer;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestInventoryApiApplication {
+
+	@Bean
+	@ServiceConnection
+	MySQLContainer<?> mysqlContainer() {
+		return new MySQLContainer<>("mysql:latest");
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.from(InventoryApiApplication::main).with(TestInventoryApiApplication.class).run(args);
+	}
+
+}
