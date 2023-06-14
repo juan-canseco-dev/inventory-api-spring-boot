922633c22ebca3e019fd464e9d8c7cb6238221ac
Juan Pablo Canseco Rios <juan.canseco.rios@gmail.com>
2023-06-14 14:35:38 -0600

    Add unit of measurement mapper

---
+++ src/main/java/com/jcanseco/inventoryapi/mappers/UnitOfMeasurementMapper.java
@@ -0,0 +1,14 @@
+package com.jcanseco.inventoryapi.mappers;
+
+import com.jcanseco.inventoryapi.dtos.unitofmeasurements.CreateUnitOfMeasurementDto;
+import com.jcanseco.inventoryapi.dtos.unitofmeasurements.UnitOfMeasurementDto;
+import com.jcanseco.inventoryapi.entities.UnitOfMeasurement;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface UnitOfMeasurementMapper {
+    UnitOfMeasurementDto entityToDto(UnitOfMeasurement entity);
+    @Mapping(target = "id", ignore = true)
+    UnitOfMeasurement createDtoToEntity(CreateUnitOfMeasurementDto dto);
+}
