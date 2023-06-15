c477e48914e0a69a4483051693d01d0f8d755b58
Juan Pablo Canseco Rios <juan.canseco.rios@gmail.com>
2023-06-14 20:40:32 -0600

    Add findByName methos in UnitOfMeasurementRepository

--- src/main/java/com/jcanseco/inventoryapi/repositories/UnitOfMeasurementRepository.java
+++ src/main/java/com/jcanseco/inventoryapi/repositories/UnitOfMeasurementRepository.java
@@ -1,9 +1,14 @@
 package com.jcanseco.inventoryapi.repositories;
 
 import com.jcanseco.inventoryapi.entities.UnitOfMeasurement;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface UnitOfMeasurementRepository extends JpaRepository<UnitOfMeasurement, Long> {
+    List<UnitOfMeasurement> findAllByNameContainingOrderByName(String name);
+    Page<UnitOfMeasurement> findAllByNameContainingOrderByName(String name, Pageable pageable);
 }
