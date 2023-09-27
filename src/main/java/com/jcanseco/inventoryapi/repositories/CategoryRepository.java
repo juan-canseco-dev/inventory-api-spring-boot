549acb933d4b44b4b76ac80a2c788924ee642a0a
Juan Pablo Canseco Rios <juan.canseco.rios@gmail.com>
2023-09-26 19:24:11 -0600

    add find methods in SupplierRepository

--- src/main/java/com/jcanseco/inventoryapi/repositories/CategoryRepository.java
+++ src/main/java/com/jcanseco/inventoryapi/repositories/CategoryRepository.java
@@ -4,10 +4,10 @@ import com.jcanseco.inventoryapi.entities.Category;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Repository;
 import java.util.List;
 
-@Service
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long>
 {
     List<Category> findAllByNameContainingOrderByName(String name);
--- src/main/java/com/jcanseco/inventoryapi/repositories/SupplierRepository.java
+++ src/main/java/com/jcanseco/inventoryapi/repositories/SupplierRepository.java
@@ -1,8 +1,27 @@
 package com.jcanseco.inventoryapi.repositories;
 
 import com.jcanseco.inventoryapi.entities.Supplier;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
-public interface SupplierRepository extends JpaRepository<Supplier, Long> { }
+public interface SupplierRepository extends JpaRepository<Supplier, Long>
+{
+    List<Supplier> findAllByCompanyNameContainingOrContactNameContainingOrContactPhoneContaining(
+            String companyName,
+            String contactName,
+            String contactPhone,
+            Sort sort
+    );
+
+    Page<Supplier> findAllByCompanyNameContainingOrContactNameContainingOrContactPhoneContaining(
+            String companyName,
+            String contactName,
+            String contactPhone,
+            Pageable pageable
+    );
+}
