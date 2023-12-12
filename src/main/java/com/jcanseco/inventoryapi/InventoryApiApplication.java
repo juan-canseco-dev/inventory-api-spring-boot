69540da29fc42dcb4e00804bdfcb5a150b5c89be
Juan Pablo Canseco Rios <juan.canseco.rios@gmail.com>
2023-12-11 20:50:13 -0600

    Categories persistence tests refactor

--- src/main/java/com/jcanseco/inventoryapi/InventoryApiApplication.java
+++ src/main/java/com/jcanseco/inventoryapi/InventoryApiApplication.java
@@ -1,9 +1,5 @@
 package com.jcanseco.inventoryapi;
 
-import com.jcanseco.inventoryapi.entities.Category;
-import com.jcanseco.inventoryapi.repositories.CategoryRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
@@ -11,20 +7,8 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import java.util.List;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-public class InventoryApiApplication implements CommandLineRunner {
+public class InventoryApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryApiApplication.class, args);
-	}
-	@Autowired
-	private CategoryRepository repository;
-	@Override
-	public void run(String... args) throws Exception {
-		var categories = List.of(
-				Category.builder().name("Electronics").build(),
-				Category.builder().name("Video").build(),
-				Category.builder().name("Video Games").build()
-		);
-
-		repository.saveAllAndFlush(categories);
 	}
 }
--- src/test/java/com/jcanseco/inventoryapi/persistence/CategoriesTests.java
+++ src/test/java/com/jcanseco/inventoryapi/persistence/CategoriesTests.java
@@ -3,6 +3,7 @@ package com.jcanseco.inventoryapi.persistence;
 import com.jcanseco.inventoryapi.entities.Category;
 import com.jcanseco.inventoryapi.repositories.CategoryRepository;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,30 @@ public class CategoriesTests {
 
     @Autowired
     private CategoryRepository repository;
+
+    private Category createCategory(String name) {
+        return Category.builder()
+                .name(name)
+                .build();
+    }
+
+    @BeforeEach
+    public void setup() {
+        var categories = List.of(
+                createCategory("Electronics"),
+                createCategory("Clothing"),
+                createCategory("Home and Garden"),
+                createCategory("Books"),
+                createCategory("Sports and Outdoors"),
+                createCategory("Health and Beauty"),
+                createCategory("Toys and Games"),
+                createCategory("Automotive"),
+                createCategory("Furniture"),
+                createCategory("Music")
+        );
+        this.repository.saveAllAndFlush(categories);
+    }
+
     @AfterEach
     public void cleanup() {
         repository.deleteAll();
@@ -26,7 +51,7 @@ public class CategoriesTests {
     @Test
     public void createCategoryWhenValidCategoryReturnSavedCategoryWithGeneratedId() {
 
-        var category = Category.builder().name("Electronics").build();
+        var category = createCategory("New category");
         var newCategory = repository.saveAndFlush(category);
 
         assertTrue(newCategory.getId() > 0);
@@ -39,66 +64,34 @@ public class CategoriesTests {
 
     @Test
     public void findAllCategoriesByNameWhenNameIsEmptyShouldReturnList() {
-
-        var categories = List.of(
-                Category.builder().name("Electronics").build(),
-                Category.builder().name("Video Games").build()
-        );
-
-        var savedCategories = this.repository.saveAllAndFlush(categories);
         var foundCategories = repository.findAllByNameContaining("", Sort.by("name").ascending());
-        assertTrue(
-                savedCategories.size() == foundCategories.size() &&
-                        savedCategories.containsAll(foundCategories) &&
-                        foundCategories.containsAll(savedCategories)
-        );
+        assertNotNull(foundCategories);
+        assertEquals(10, foundCategories.size());
     }
 
     @Test
     public void findAllCategoriesByNameContainingWhenContains() {
-        repository.deleteAll();
-        var categories = List.of(
-                Category.builder().name("Electronics").build(),
-                Category.builder().name("Video Games").build()
-        );
-        this.repository.saveAllAndFlush(categories);
-        var foundCategories = repository.findAllByNameContaining("video", Sort.by("name").ascending());
+        var foundCategories = repository.findAllByNameContaining("v", Sort.by("name").ascending());
         assertEquals(1, foundCategories.size());
     }
 
     @Test
     public void findAllPagedCategoriesByNameWhenInputIsValidShouldReturnValidPage() {
-
-        var categories = List.of(
-                Category.builder().name("Electronics").build(),
-                Category.builder().name("Video").build(),
-                Category.builder().name("Video Games").build()
-        );
-
-        repository.saveAllAndFlush(categories);
-
-        var request = PageRequest.of(0, 10);
-        var page = repository.findAllByNameContaining("vi", request);
+        var request = PageRequest.of(0, 3);
+        var page = repository.findAllByNameContaining("e", request);
 
         assertNotNull(page.getContent());
-        assertEquals(2, page.getContent().size());
+        assertEquals(3, page.getContent().size());
+        assertEquals(2, page.getTotalPages());
+        assertEquals(6, page.getTotalElements());
     }
 
     @Test
     public void findAllPagedCategoriesWhenNameIsEmptyShouldReturnValidPageWithAllItems() {
-        var categories = List.of(
-                Category.builder().name("Electronics").build(),
-                Category.builder().name("Video").build(),
-                Category.builder().name("Video Games").build()
-        );
-
-        repository.saveAllAndFlush(categories);
-
         var request = PageRequest.of(0, 10);
         var page = repository.findAllByNameContaining("", request);
 
         assertNotNull(page.getContent());
-        assertEquals(3, page.getContent().size());
+        assertEquals(10, page.getContent().size());
     }
-
 }
