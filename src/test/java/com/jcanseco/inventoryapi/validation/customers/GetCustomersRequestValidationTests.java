de4ec6b957e61921ad0f5d345ca764a4262edecf
Juan Pablo Canseco Rios <juan.canseco.rios@gmail.com>
2024-01-01 18:20:05 -0600

    Get Customers Request Validation Tests

--- src/test/java/com/jcanseco/inventoryapi/validation/customers/GetCustomersRequestValidationTests.java
+++ src/test/java/com/jcanseco/inventoryapi/validation/customers/GetCustomersRequestValidationTests.java
@@ -1,4 +1,123 @@
 package com.jcanseco.inventoryapi.validation.customers;
 
+import com.jcanseco.inventoryapi.dtos.customers.GetCustomersRequest;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class GetCustomersRequestValidationTests {
+
+    private Validator validator;
+
+    @BeforeEach
+    public void setup() {
+        var factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    @Test
+    public void getCustomersRequestWhenFieldsAreNotPresentValidationShouldNotFail() {
+        var request = GetCustomersRequest.builder().build();
+        var violations = validator.validate(request);
+        assertTrue(violations.isEmpty());
+    }
+
+    @Test
+    public void getCustomersRequestWhenAllFieldsArePresentValidationShouldNotFail() {
+        var request = GetCustomersRequest.builder()
+                .dni("1")
+                .fullName("a")
+                .phone("c")
+                .pageNumber(1)
+                .pageSize(1)
+                .orderBy("fullName")
+                .sortOrder("asc")
+                .build();
+        var violations = validator.validate(request);
+        assertTrue(violations.isEmpty());
+    }
+
+    @Test
+    public void getCustomersRequestWhenPageNumberIsLessThanOneValidationShouldFail() {
+        var request = GetCustomersRequest.builder()
+                .pageNumber(0)
+                .pageSize(1)
+                .orderBy("fullName")
+                .sortOrder("asc")
+                .build();
+        var violations = validator.validate(request);
+        assertFalse(violations.isEmpty());
+        assertEquals(1, violations.size());
+    }
+
+    @Test
+    public void getCustomersRequestWhenPageSizeIsLessThanOneValidationShouldFail() {
+        var request = GetCustomersRequest.builder()
+                .pageNumber(1)
+                .pageSize(0)
+                .orderBy("fullName")
+                .sortOrder("asc")
+                .build();
+        var violations = validator.validate(request);
+        assertFalse(violations.isEmpty());
+        assertEquals(1, violations.size());
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(strings = {"id", "dni", "fullName", "phone"})
+    public void getCustomersRequestWhenOrderByIsValidValidationShouldNotFail(String orderBy) {
+        var request = GetCustomersRequest.builder()
+                .pageNumber(1)
+                .pageSize(1)
+                .orderBy(orderBy)
+                .sortOrder("asc")
+                .build();
+        var violations = validator.validate(request);
+        assertTrue(violations.isEmpty());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"invalid_field"})
+    public void getCustomersRequestWhenOrderByIsInvalidValidValidationShouldFail(String orderBy) {
+        var request = GetCustomersRequest.builder()
+                .pageNumber(1)
+                .pageSize(1)
+                .orderBy(orderBy)
+                .sortOrder("asc")
+                .build();
+        var violations = validator.validate(request);
+        assertFalse(violations.isEmpty());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"asc", "ascending", "desc", "descending"})
+    public void getCustomersRequestWhenSortOrderIsValidValidationShouldNotFail(String sortOrder) {
+        var request = GetCustomersRequest.builder()
+                .pageNumber(1)
+                .pageSize(1)
+                .orderBy("fullName")
+                .sortOrder(sortOrder)
+                .build();
+        var violations = validator.validate(request);
+        assertTrue(violations.isEmpty());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"asce", "ascendingf", "desce", "descendingf"})
+    public void getCustomersRequestWhenSortOrderIsInvalidValidationShouldFail(String sortOrder) {
+        var request = GetCustomersRequest.builder()
+                .pageNumber(1)
+                .pageSize(1)
+                .orderBy("fullName")
+                .sortOrder(sortOrder)
+                .build();
+        var violations = validator.validate(request);
+        assertFalse(violations.isEmpty());
+    }
+
 }
