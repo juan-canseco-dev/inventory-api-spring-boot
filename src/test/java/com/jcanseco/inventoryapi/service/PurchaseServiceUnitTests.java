57e306d2ca4466583db2cd813cdfdaae799c8dbc
Juan Pablo Canseco Rios <juan.canseco.rios@gmail.com>
2024-01-21 23:44:57 -0600

    Purchase Service Unit Tests for create and update methods

--- src/test/java/com/jcanseco/inventoryapi/service/PurchaseServiceUnitTests.java
+++ src/test/java/com/jcanseco/inventoryapi/service/PurchaseServiceUnitTests.java
@@ -1,6 +1,10 @@
 package com.jcanseco.inventoryapi.service;
 
+import com.jcanseco.inventoryapi.dtos.purchases.CreatePurchaseDto;
+import com.jcanseco.inventoryapi.dtos.purchases.UpdatePurchaseDto;
 import com.jcanseco.inventoryapi.entities.*;
+import com.jcanseco.inventoryapi.exceptions.DomainException;
+import com.jcanseco.inventoryapi.exceptions.NotFoundException;
 import com.jcanseco.inventoryapi.mappers.PurchaseMapper;
 import com.jcanseco.inventoryapi.repositories.ProductRepository;
 import com.jcanseco.inventoryapi.repositories.PurchaseRepository;
@@ -9,25 +13,16 @@ import com.jcanseco.inventoryapi.repositories.SupplierRepository;
 import com.jcanseco.inventoryapi.services.PurchaseService;
 import com.jcanseco.inventoryapi.utils.IndexUtility;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import com.jcanseco.inventoryapi.utils.IndexUtility;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mapstruct.factory.Mappers;
-import org.mockito.*;
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import org.mockito.*;
 import static org.mockito.Mockito.*;
-
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 public class PurchaseServiceUnitTests {
@@ -46,15 +41,19 @@ public class PurchaseServiceUnitTests {
     @InjectMocks
     private PurchaseService service;
     private final Long purchaseId = 1L;
-    private final Long supplier1Id = 1L;
-    private final Long supplier2Id = 2L;
+    private final Long supplierId = 1L;
+
     private final HashMap<Long, Long> productsWithQuantities = new HashMap<>() {{
         put(9L, 10L);
         put(10L, 10L);
     }};
 
-    private Supplier supplier1;
-    private Supplier supplier2;
+    private final HashMap<Long, Long> productsWithQuantitiesForUpdate = new HashMap<>() {{
+        put(9L, 5L);
+        put(10L, 5L);
+    }};
+
+    private Supplier supplier;
     private List<Product> products;
 
     @BeforeEach
@@ -78,26 +77,18 @@ public class PurchaseServiceUnitTests {
                 .street("123 Main St")
                 .build();
 
-        supplier1 = Supplier.builder()
-                .id(supplier1Id)
+        supplier = Supplier.builder()
+                .id(supplierId)
                 .companyName("ABC Corp")
                 .contactName("John Doe")
                 .contactPhone("555-1234-1")
                 .address(address)
                 .build();
 
-        supplier2 = Supplier.builder()
-                .id(supplier2Id)
-                .companyName("Tech Solutions Inc")
-                .contactName("Alice Brown")
-                .contactPhone("555-1234-4")
-                .address(address)
-                .build();
-
         products = List.of(
                 Product.builder()
                         .id(9L)
-                        .supplier(supplier1)
+                        .supplier(supplier)
                         .category(category)
                         .unit(unit)
                         .name("Vacuum Cleaner")
@@ -106,7 +97,7 @@ public class PurchaseServiceUnitTests {
                         .build(),
                 Product.builder()
                         .id(10L)
-                        .supplier(supplier1)
+                        .supplier(supplier)
                         .category(category)
                         .unit(unit)
                         .name("Toaster")
@@ -119,26 +110,101 @@ public class PurchaseServiceUnitTests {
     @Test
     public void createPurchaseShouldReturnProductId() {
 
+        var savedPurchase = Purchase.createNew(supplier, products, productsWithQuantities);
+        savedPurchase.setId(purchaseId);
+
+        var dto = CreatePurchaseDto.builder()
+                .supplierId(supplierId)
+                .productsWithQuantities(productsWithQuantities)
+                .build();
+
+        when(supplierRepository.findById(supplierId)).thenReturn(Optional.of(supplier));
+        when(productRepository.findAllById(productsWithQuantities.keySet())).thenReturn(products);
+        when(purchaseRepository.saveAndFlush(any())).thenReturn(savedPurchase);
+
+        var resultId = service.createPurchase(dto);
+        assertEquals(purchaseId, resultId);
+
+        verify(purchaseRepository, times(1)).saveAndFlush(any());
+
+        var purchaseArgCaptor = ArgumentCaptor.forClass(Purchase.class);
+        verify(purchaseRepository).saveAndFlush(purchaseArgCaptor.capture());
+
+        var newPurchase = purchaseArgCaptor.getValue();
+        assertNotNull(newPurchase);
+
+        assertEquals(savedPurchase.getSupplier(), newPurchase.getSupplier());
+        assertEquals(savedPurchase.getItems(), newPurchase.getItems());
+        assertEquals(savedPurchase.getTotal(), newPurchase.getTotal());
     }
 
     @Test
     public void createPurchaseWhenSupplierDoNotExistsShouldThrowException() {
-
+        when(supplierRepository.findById(supplierId)).thenReturn(Optional.empty());
+        var dto = CreatePurchaseDto.builder()
+                .supplierId(supplierId)
+                .productsWithQuantities(productsWithQuantities)
+                .build();
+        assertThrows(DomainException.class, () -> service.createPurchase(dto));
     }
 
     @Test
     public void updatePurchaseWhenPurchaseExistsAndIsNotArrivedShouldUpdate() {
 
+        var dto = UpdatePurchaseDto.builder()
+                .purchaseId(purchaseId)
+                .productsWithQuantities(productsWithQuantitiesForUpdate)
+                .build();
+
+        var foundPurchase = Purchase.createNew(supplier, products, productsWithQuantities);
+        foundPurchase.setId(purchaseId);
+
+        var expectedPurchase = Purchase.createNew(supplier, products, productsWithQuantities);
+        expectedPurchase.setId(purchaseId);
+        expectedPurchase.update(products, productsWithQuantitiesForUpdate);
+
+        when(purchaseRepository.findById(purchaseId)).thenReturn(Optional.of(foundPurchase));
+        when(productRepository.findAllById(productsWithQuantitiesForUpdate.keySet())).thenReturn(products);
+        when(purchaseRepository.saveAndFlush(any())).thenReturn(any());
+
+        service.updatePurchase(dto);
+
+        verify(purchaseRepository, times(1)).saveAndFlush(any());
+
+        var purchaseArgCaptor = ArgumentCaptor.forClass(Purchase.class);
+        verify(purchaseRepository).saveAndFlush(purchaseArgCaptor.capture());
+
+        var updatedPurchase = purchaseArgCaptor.getValue();
+        assertNotNull(updatedPurchase);
+        assertEquals(expectedPurchase, updatedPurchase);
     }
 
     @Test
     public void updatePurchaseWhenPurchaseIsArrivedShouldThrowException() {
 
+        var dto = UpdatePurchaseDto.builder()
+                .purchaseId(purchaseId)
+                .productsWithQuantities(productsWithQuantitiesForUpdate)
+                .build();
+
+        var foundPurchase = Purchase.createNew(supplier, products, productsWithQuantities);
+        foundPurchase.setId(purchaseId);
+        foundPurchase.markAsArrived();
+
+        when(purchaseRepository.findById(purchaseId)).thenReturn(Optional.of(foundPurchase));
+        when(productRepository.findAllById(productsWithQuantitiesForUpdate.keySet())).thenReturn(products);
+
+        assertThrows(DomainException.class, () -> service.updatePurchase(dto));
     }
 
     @Test
     public void updatePurchaseWhenPurchaseNotExistsShouldThrowException() {
-
+        var dto = UpdatePurchaseDto.builder()
+                .purchaseId(purchaseId)
+                .productsWithQuantities(productsWithQuantitiesForUpdate)
+                .build();
+        when(purchaseRepository.findById(purchaseId)).thenReturn(Optional.empty());
+        assertThrows(NotFoundException.class, () -> service.updatePurchase(dto));
     }
 
     @Test
