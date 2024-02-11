d5ee43a4aae1407c3c13ae61bd26c42e855ca23b
Juan Pablo Canseco Rios <juan.canseco.rios@gmail.com>
2024-02-10 20:57:43 -0600

    Refactor Resource Integration Tests

---
+++ src/test/java/com/jcanseco/inventoryapi/integration/ResourceControllerIntegrationTests.java
@@ -0,0 +1,33 @@
+package com.jcanseco.inventoryapi.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.*;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class ResourceControllerIntegrationTests {
+    @Autowired
+    private MockMvc mockMvc;
+    @WithMockUser
+    @Test
+    public void getResourcesStatusShouldBeOk() throws Exception {
+        mockMvc.perform(
+                        get("/api/resources")
+                                .accept(MediaType.APPLICATION_JSON)
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$", hasSize(9)));
+    }
+}
